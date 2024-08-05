d5e05205b5329a6780ded1204eda91628c993822
unknown <fuadsami5@gmail.com>
2024-08-06 01:31:46 +0300

    Update auth/[...nextauth]/route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -6,7 +6,7 @@ import clientPromise from '../../../../lib/mongodb'
 
 const adminEmails = ['fuad.nasseraldeen@gmail.com']
 
-export const authOptions = {
+const authOptions = {
   secret: process.env.SECRET,
   providers: [
     GoogleProvider({
